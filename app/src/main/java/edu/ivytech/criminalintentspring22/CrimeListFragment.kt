1f8101d4852c368a8264ac8ec1adda49f11274a0
Angela Venable <avenable11@ivytech.edu>
2022-03-02 21:40:08 -0500

    Adapter

--- app/src/main/java/edu/ivytech/criminalintentspring22/CrimeListFragment.kt
+++ app/src/main/java/edu/ivytech/criminalintentspring22/CrimeListFragment.kt
@@ -44,6 +44,22 @@ class CrimeListFragment : Fragment() {
             Toast.makeText(context, "${crime.title} pressed", Toast.LENGTH_SHORT).show()
         }
     }
+    private inner class CrimeAdapter(var crimes : List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = ListItemCrimeBinding.inflate(layoutInflater, parent, false)
+            return CrimeHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.bind(crime)
+        }
+
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
