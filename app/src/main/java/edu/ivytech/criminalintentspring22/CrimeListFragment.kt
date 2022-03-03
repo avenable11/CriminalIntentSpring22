72ed1a59efc71da899c3c23f0b226ab17e6fdcd5
Angela Venable <avenable11@ivytech.edu>
2022-03-02 21:33:56 -0500

    View Holder

--- app/src/main/java/edu/ivytech/criminalintentspring22/CrimeListFragment.kt
+++ app/src/main/java/edu/ivytech/criminalintentspring22/CrimeListFragment.kt
@@ -1,13 +1,17 @@
 package edu.ivytech.criminalintentspring22
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.ivytech.criminalintentspring22.databinding.FragmentCrimeListBinding
+import edu.ivytech.criminalintentspring22.databinding.ListItemCrimeBinding
 
 class CrimeListFragment : Fragment() {
     private var _binding: FragmentCrimeListBinding? = null
@@ -23,6 +27,23 @@ class CrimeListFragment : Fragment() {
         return binding.root
     }
 
+    private inner class CrimeHolder (val itemBinding:ListItemCrimeBinding) :
+        RecyclerView.ViewHolder(itemBinding.root), View.OnClickListener {
+            private lateinit var crime : Crime
+            init {
+                itemBinding.root.setOnClickListener(this)
+            }
+            fun bind(crime: Crime) {
+                this.crime = crime
+                itemBinding.crimeTitleList.text = crime.title
+                itemBinding.crimeDateList.text = DateFormat.format("EEEE, MMM dd, yyyy", crime.date)
+                itemBinding.solvedImageList.visibility = if(crime.isSolved) View.VISIBLE else View.INVISIBLE
+            }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context, "${crime.title} pressed", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
